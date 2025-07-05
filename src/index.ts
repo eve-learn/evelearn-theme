c84b2b3466243511e1aa9f96e2436a31d73b4b5c
vidman22 <29338715+vidman22@users.noreply.github.com>
2025-07-05 07:45:12 -0700

    split up index and server

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,27 @@ export { default as Fade } from './Fade/Fade';
 export { default as Backdrop } from './Backdrop/Backdrop';
 export { default as Breakpoint } from './Breakpoint/Breakpoint';
 
+export { default as Overlay } from './Backdrop/Overlay';
+export { default as Button } from './Button/Button';
+export { default as FunButton } from './Button/FunButton';
+export { default as FunRoundButton } from './Button/FunRoundButton';
+export { default as Checkbox } from './Checkbox/Checkbox';
+export { default as RadioOption } from './Checkbox/RadioOption';
+export { default as BottomAlert } from './Common/BottomAlert';
+export { default as IconInfo } from './Common/IconInfo';
+export { default as Modal } from './Common/Modal';
+export { default as NoticeBox } from './Common/NoticeBox';
+export { default as RatingStars } from './Common/RatingStars';
+export { default as Slider } from './Common/Slider';
+export { default as Tippy } from './Common/Tippy';
+export { default as ToggleSwitch } from './Common/ToggleSwitch';
+export { default as UserContentSwitcher } from './Common/UserContentSwitcher';
+export { default as ProgressBarSimple } from './ProgressBar/ProgressBarSimple';
+export { default as ProgressBar } from './ProgressBar/ProgressBar';
+export { default as CircleProgress } from './ProgressBar/CircleProgress';
+export { default as CourseProgress } from './ProgressBar/CourseProgress';
+export { default as ScrollProgress } from './ProgressBar/ScrollProgress';
+export { default as OverlaySpinner } from './Spinners/OverlaySpinner';
+export { default as AnimateLogo } from './Icons/AnimateLogo';
+
 
