8114d4673e0d39043ad01ea20f170152ef38de5b
vidman22 <29338715+vidman22@users.noreply.github.com>
2025-07-27 19:10:59 -0700

    step component style

--- src/Common/StepsComponent.tsx
+++ src/Common/StepsComponent.tsx
@@ -14,7 +14,7 @@ type Props = {
 const StepsComponent = ({ steps, activeStep, onStepPress }: Props) => {
 
     return (
-        <div className='flex justify-between items-center w-full p-1 mb-2'>
+        <div className='flex justify-between items-center space-x-1 w-full p-1 mb-2'>
             {steps.map((step, index) => {
                 return (
                     <div key={`step_${index}_${step.label}`} className='flex justify-center items-center space-x-2 w-full'>
@@ -28,7 +28,7 @@ const StepsComponent = ({ steps, activeStep, onStepPress }: Props) => {
                                         : 'bg-gray-100 dark:bg-slate-800 text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-slate-700 border-2 border-transparent hover:border-primary/30'
                                     }`}
                             >
-                                <p className='font-medium truncate'><span className='text-gray-500 dark:text-gray-400'>{index + 1}. </span>{step.label}</p>
+                                <p className='font-medium truncate'><span className='text-semibold'>{index + 1}. </span>{step.label}</p>
                             </button>
                         </div>
                     </div>
