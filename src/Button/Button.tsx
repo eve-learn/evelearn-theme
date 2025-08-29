00ac52c9518a5c238c94837bf0ba9519ac8f29d5
vidman22 <29338715+vidman22@users.noreply.github.com>
2025-08-29 10:25:37 -0700

    fix overlay and button

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -18,23 +18,23 @@ const Button = ({ disabled = false, loading, onClick, kind = 'primary', size = '
     const styled = () => {
         switch (kind) {
             case 'primary':
-                return `bg-primary font-header text-white font-medium transition-all duration-75 ${disabled ? 'bg-white/50' : 'hover:bg-black/10'}`;
+                return `bg-primary font-header text-white font-medium transition-all duration-75 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-black/10'}`;
             case 'indigo':
-                return `bg-indigo-500 font-header text-white font-medium transition-all duration-75 ${disabled ? 'bg-white/50' : 'hover:bg-black/10'}`;
+                return `bg-indigo-500 font-header text-white font-medium transition-all duration-75 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-black/10'}`;
             case 'green':
-                return `bg-green-500 font-header text-white font-medium transition-all duration-75 ${disabled ? 'bg-white/50' : 'hover:bg-black/10'}`;
+                return `bg-green-500 font-header text-white font-medium transition-all duration-75 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-black/10'}`;
             case 'teal':
-                return `bg-teal-500 font-header text-white font-medium transition-all duration-75 ${disabled ? 'bg-white/50' : 'hover:bg-black/10'}`;
+                return `bg-teal-500 font-header text-white font-medium transition-all duration-75 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-black/10'}`;
             case 'secondary':
-                return 'bg-secondary text-white font-header font-medium hover:bg-black/10 transition-all duration-75';
+                return `bg-secondary text-white font-header font-medium transition-all duration-75 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-black/10'}`;
             case 'base':
-                return 'bg-white border-1 font-header border-gray-200 dark:bg-slate-600 dark:text-gray-100 dark:hover:bg-slate-400 hover:bg-slate-100 transition-all duration-75';
+                return `bg-white border-1 font-header border-gray-200 dark:bg-slate-600 dark:text-gray-100 transition-all duration-75 ${disabled ? 'opacity-50 cursor-not-allowed' : 'dark:hover:bg-slate-400 hover:bg-slate-100'}`;
             case 'gray':
-                return 'bg-slate-400 dark:bg-slate-700 font-header text-white border-1 font-medium hover:bg-black/10 transition-opacity duration-75';
+                return `bg-slate-400 dark:bg-slate-700 font-header text-white border-1 font-medium transition-opacity duration-75 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-black/10'}`;
             case 'alert':
-                return 'bg-amber-500 font-header text-white transition-all hover:bg-black/10 duration-75';
+                return `bg-amber-500 font-header text-white transition-all duration-75 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-black/10'}`;
             case 'warning':
-                return 'bg-red-500 font-header text-white font-medium hover:bg-black/10 transition-opacity duration-100';
+                return `bg-red-500 font-header text-white font-medium transition-opacity duration-100 ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-black/10'}`;
             default:
                 break;
         }
--- src/Spinners/OverlaySpinner.tsx
+++ src/Spinners/OverlaySpinner.tsx
@@ -8,12 +8,8 @@ type Props = { visible: boolean; };
 
 const OverlaySpinner = ({ visible }: Props) => {
     const [modalRoot, setModalRoot] = useState<HTMLElement | null>(null);
-    const [mounted, setMounted] = useState(false);
-    useEffect(() => setMounted(true), []);
 
     useEffect(() => {
-        if (modalRoot || !visible) return;
-        // Create a div element to serve as our portal's root
         let root = document.getElementById('spinner-root');
         if (!root) {
             root = document.createElement('div');
@@ -21,30 +17,24 @@ const OverlaySpinner = ({ visible }: Props) => {
             document.body.appendChild(root);
         }
         setModalRoot(root);
-        return () => {
-            let root = document.getElementById('spinner-root');
-            if (root) {
-                // root = document.createElement('div');
-                // root.id = 'modal-root';
-                document.body.removeChild(root);
-            }
-        }
+        
+        // No cleanup needed here. We want the root to persist.
+    }, []);
 
-    }, [visible]);
+    if (!visible || !modalRoot) {
+        return null;
+    }
 
-    if (!visible || !mounted) return null;
-    return (
-        createPortal(
-            <div style={{zIndex: 11000}} className="animate-fade-in">
-                <div style={{zIndex: 11001}} className='fixed top-0 left-0 w-full h-screen flex items-center justify-center dark:bg-black/70 bg-white/60'>
-                    <div className='h-44 w-44 overflow-visible'>
-                        <AnimateLogo />
-                    </div>
+    return createPortal(
+        <div style={{zIndex: 11000}} className="animate-fade-in">
+            <div style={{zIndex: 11001}} className='fixed top-0 left-0 w-full h-screen flex items-center justify-center dark:bg-black/70 bg-white/60'>
+                <div className='h-44 w-44 overflow-visible'>
+                    <AnimateLogo />
                 </div>
-            </div>,
-            typeof window !== 'undefined' ? document.body : {} as DocumentFragment, 'spinner'
-        )
-    )
-}
+            </div>
+        </div>,
+        modalRoot
+    );
+};
 
 export default OverlaySpinner;
